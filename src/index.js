9a415ce3d8031a42a08d1422acba9922bb8b4dd5
vayntail <113714157+vayntail@users.noreply.github.com>
2024-11-07 19:50:07 -0500

    Validation for username and email finished

---
+++ src/index.js
@@ -0,0 +1,74 @@
+
+const form = document.getElementById('registration');
+
+const usernameEl = form.elements['username'];
+const emailEl = form.elements['email'];
+const passwordEl = form.elements['password'];
+
+form.addEventListener('submit', validate);
+
+function validate(evt) {
+    if (!validateUsername()) {
+        evt.returnValue = false;
+        return false;
+    }
+    else if (!validateEmail()) {
+        evt.returnValue = false;
+        return false;
+    }
+    return true;
+}
+
+function validateUsername(){
+    const regex = /[^A-Za-z0-9]/;
+    let username = usernameEl.value;
+    let isUnique = false;
+    let containsSpecial = false;
+    for (let i=0; i < username.length; i++) {
+        if (username[i] != username[0]) isUnique = true;
+    }
+
+    if (username.length == 0) {
+        alert("Your username cannot be blank.");
+        usernameEl.focus();
+        return false;
+    }
+    else if (username.length <= 4) {
+        alert("Your username must be at least 4 characters long.");
+        usernameEl.focus();
+        return false;
+    }
+    else if (!isUnique) {
+        alert("Your username must have at least two unique characters.");
+        usernameEl.focus();
+        return false;
+    }
+    else if (regex.test(username)){
+        alert("Your username cannot contain any special characters or whitespace.");
+        usernameEl.focus();
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+function validateEmail() {
+    let email = emailEl.value;
+    console.log(email)
+    if (!email.includes("@") && !email.includes(".")) {
+        alert(`Please enter a valid email address.`);
+        emailEl.focus();
+        return false;
+    }
+    else if (email.includes("@example.com")){
+        alert(`Your email must not be from the domain "example.com."`);
+        emailEl.focus();
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+
